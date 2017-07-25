42d687d9a37b30db81c86e2b7ec826bceac5e30f
Avinash Tadavarthy <avinash.tadavarthy@gmail.com>
2017-07-25 18:32:46 +0530

    Initial commit

---
+++ app/src/main/java/com/androidapps/avinashtadavarthy/moviereview/Registration3.java
@@ -0,0 +1,62 @@
+package com.androidapps.avinashtadavarthy.moviereview;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+public class Registration3 extends AppCompatActivity {
+
+    ImageButton nextpg;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration3);
+
+        nextpg = (ImageButton) findViewById(R.id.nextpg);
+
+        nextpg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent next = new Intent(getApplicationContext(),Registration4.class);
+                startActivity(next);
+            }
+        });
+    }
+
+
+    //keyboard disappears when you click outside
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        View v = getCurrentFocus();
+
+        if (v != null &&
+                (ev.getAction() == MotionEvent.ACTION_UP || ev.getAction() == MotionEvent.ACTION_MOVE) &&
+                v instanceof EditText &&
+                !v.getClass().getName().startsWith("android.webkit.")) {
+            int scrcoords[] = new int[2];
+            v.getLocationOnScreen(scrcoords);
+            float x = ev.getRawX() + v.getLeft() - scrcoords[0];
+            float y = ev.getRawY() + v.getTop() - scrcoords[1];
+
+            if (x < v.getLeft() || x > v.getRight() || y < v.getTop() || y > v.getBottom())
+                hideKeyboard(this);
+        }
+        return super.dispatchTouchEvent(ev);
+    }
+
+    public static void hideKeyboard(Activity activity) {
+        if (activity != null && activity.getWindow() != null && activity.getWindow().getDecorView() != null) {
+            InputMethodManager imm = (InputMethodManager)activity.getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(activity.getWindow().getDecorView().getWindowToken(), 0);
+        }
+    }
+
+}
---
+++ app/src/main/java/com/androidapps/avinashtadavarthy/moviereview/Registration4.java
@@ -0,0 +1,13 @@
+package com.androidapps.avinashtadavarthy.moviereview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Registration4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration4);
+    }
+}
